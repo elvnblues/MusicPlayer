dae00f9ad4f87df96f4b927921d75831469a7b03
elvnblues <elvnblues@gmail.com>
2014-12-31 10:59:03 +0800

    2014/12/31 MusicPlayer
    2014/12/31 MusicPlayer

---
+++ src/com/hicsg/musicplayer/MusicFilter.java
@@ -0,0 +1,15 @@
+package com.hicsg.musicplayer;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class MusicFilter implements FilenameFilter{
+
+    @Override
+    public boolean accept(File dir, String filename) {
+        // TODO Auto-generated method stub
+        return (filename.endsWith(".mp3"));
+    }
+
+     
+}
